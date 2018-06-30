dc0b04add4ad2fb47fbe8847ba361c480a36ecdc
bboyKamel <nacoon@tlen.pl>
2018-06-30 14:31:23 +0200

    trsc zadania w komentarzu

--- src/main/java/NewPath/newMain.java
+++ src/main/java/NewPath/newMain.java
@@ -1,4 +1,10 @@
 package NewPath;
 
 public class newMain {
+    public static void main (String [] args){
+        String xyz = "rozbudowa";
+        // zamien pierwsza litere z ostatnia
+        System.out.println(xyz.toUpperCase());
+
+    }
 }
